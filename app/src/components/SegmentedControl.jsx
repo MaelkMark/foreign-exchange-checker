5cbe297a7f2b1560aba4663612caa11b8730c13b
MaelkMark <110915409+MaelkMark@users.noreply.github.com>
2026-06-29 14:50:11 +0200

    Added a new reusable SegmentedControl component

---
+++ app/src/components/SegmentedControl.jsx
@@ -0,0 +1,30 @@
+import { ToggleButtonGroup, ToggleButton, SelectionIndicator } from "react-aria-components/ToggleButtonGroup";
+import clsx from "clsx";
+
+import "./SegmentedControl.css";
+
+export default function SegmentedControl({ children, selectedOption, onSelect, className = null, ...props }) {
+    return (
+        <ToggleButtonGroup
+            className={clsx("segmented-control", className)}
+            disallowEmptySelection
+            selectionMode="single"
+            selectedKeys={selectedOption}
+            onSelectionChange={onSelect}
+            {...props}
+        >
+            {children}
+        </ToggleButtonGroup>
+    );
+}
+
+function Item({ children, id, className = null, ...props }) {
+    return (
+        <ToggleButton id={id} className={clsx("segmented-control-item", className)} {...props}>
+            <SelectionIndicator className="react-aria-SelectionIndicator" data-selected />
+            <span>{children}</span>
+        </ToggleButton>
+    );
+}
+
+SegmentedControl.Item = Item;
