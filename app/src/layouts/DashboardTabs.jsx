59d6fb088d37b6d8b6de5d22e56153af661ab37a
MaelkMark <110915409+MaelkMark@users.noreply.github.com>
2026-07-01 18:39:03 +0200

    The number of favorites and logs are now displayed as notifications next to the corresponding tab labels.

--- app/src/layouts/DashboardTabs.jsx
+++ app/src/layouts/DashboardTabs.jsx
@@ -7,9 +7,12 @@ import Compare from "./tabs/Compare";
 import Favorites from "./tabs/Favorites";
 import Logs from "./tabs/Logs";
 
+import userContext from "../context/UserContext";
+
 import "./DashboardTabs.css";
 
 export default function DashboardTabs() {
+    const { favorites, logs } = React.useContext(userContext);
     return (
         <Tabs>
             <Tabs.Tab label="History" id="history">
@@ -18,10 +21,10 @@ export default function DashboardTabs() {
             <Tabs.Tab label="Compare" id="compare">
                 <Compare />
             </Tabs.Tab>
-            <Tabs.Tab label="Favorites" id="favorites">
+            <Tabs.Tab label="Favorites" id="favorites" notifications={favorites?.length || null}>
                 <Favorites />
             </Tabs.Tab>
-            <Tabs.Tab label="Logs" id="logs">
+            <Tabs.Tab label="Logs" id="logs" notifications={logs?.length || null}>
                 <Logs />
             </Tabs.Tab>
         </Tabs>
