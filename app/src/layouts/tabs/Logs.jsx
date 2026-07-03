9eb5d7c5ff4a066cbafb1d9bf29489ebc7b65c24
MaelkMark <110915409+MaelkMark@users.noreply.github.com>
2026-07-03 08:31:53 +0200

    Fixed key error in Logs

--- app/src/layouts/tabs/Logs.jsx
+++ app/src/layouts/tabs/Logs.jsx
@@ -13,11 +13,11 @@ import "./Logs.css";
 export default function Logs() {
     const { logs, setLogs } = React.useContext(UserContext);
 
-    function getDateString(timestamp) {
-        const diff = new Date().getTime() - new Date(timestamp).getTime();
+    function getDateString(datetime) {
+        const diff = new Date().getTime() - new Date(datetime).getTime();
         if (diff > 1000 * 60 * 60 * 24) {
             // more than a day ago
-            const date = new Date(timestamp);
+            const date = new Date(datetime);
             return date.toLocaleString(undefined, { month: "short", day: "numeric" });
         } else if (diff >= 1000 * 60 * 60) {
             // more than an hour ago
@@ -39,36 +39,34 @@ export default function Logs() {
                 <div className="list-header-left list-header-highlight">Conversion log</div>
                 <div className="list-header-right">
                     <div className="list-header-total">{logs?.length || 0} Logged</div>
-                    <Button className="logs-clear-all" onClick={() => setLogs([])}>Clear all</Button>
+                    <Button className="logs-clear-all" onClick={() => setLogs([])}>
+                        Clear all
+                    </Button>
                 </div>
             </div>
             <div className="list-wrapper">
                 <div className="list-items">
                     {logs
                         .sort((a, b) => new Date(b.datetime) - new Date(a.datetime))
-                        .map(log => {
-                            return (
-                                <div className="list-item" key={log.id}>
-                                    <div className="log-date">{getDateString(log.datetime)}</div>
-                                    <div className="log-currencies">
-                                        {log.sendCurrency} <ArrowRightIcon className="log-arrow" />{" "}
-                                        {log.receiveCurrency}
-                                    </div>
-                                    <div className="log-amount-send">
-                                        {amountFormat.format(log.sendAmount)}{" "}
-                                    </div>
-                                    <div className="log-amount-receive">
-                                        {amountFormat.format(log.receiveAmount)}
-                                    </div>
-                                    <Button
-                                        className="icon-button log-delete"
-                                        onClick={() => removeFromLog(logs, setLogs, log)}
-                                    >
-                                        <DeleteIcon className="log-delete-icon" />
-                                    </Button>
+                        .map(log => (
+                            <div className="list-item" key={log.datetime}>
+                                <div className="log-date">{getDateString(log.datetime)}</div>
+                                <div className="log-currencies">
+                                    {log.sendCurrency} <ArrowRightIcon className="log-arrow" />{" "}
+                                    {log.receiveCurrency}
                                 </div>
-                            );
-                        })}
+                                <div className="log-amount-send">{amountFormat.format(log.sendAmount)} </div>
+                                <div className="log-amount-receive">
+                                    {amountFormat.format(log.receiveAmount)}
+                                </div>
+                                <Button
+                                    className="icon-button log-delete"
+                                    onClick={() => removeFromLog(logs, setLogs, log)}
+                                >
+                                    <DeleteIcon className="log-delete-icon" />
+                                </Button>
+                            </div>
+                        ))}
                 </div>
             </div>
         </div>
